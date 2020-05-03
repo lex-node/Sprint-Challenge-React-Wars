afb085f3573b53b24377fae5b7f3e9b1484535e3
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-03 19:11:27 -0400

    get character list from API & set as state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+    // Try to think through what state you'll need for this app before starting. Then build out
+    // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+    // Fetch characters from the API in an effect hook. Remember, anytime you have a
+    // side effect in a component, you want to think about which state and/or props it should
+    // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.py4e.com/api/people/`)
+            .then(response => {
+                console.log(response.data.results);
+                setCharacters(response.data.results);
+            })
+            .catch(error => {
+                alert(error);
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            <h1 className="Header">Characters</h1>
+        </div>
+    );
 }
 
 export default App;
